1fab678b3276af5cb5e5c3533fa08e43a2c1bccb
Nurbolot <nubolotkadyrov06@gmail.com>
2025-10-19 16:27:27 +0600

    faet/login and out

--- src/shared/ui/logout/LogOut.tsx
+++ src/shared/ui/logout/LogOut.tsx
@@ -1,24 +1,30 @@
 import { useAuthStore } from '@/store/auth.store';
 import toast from 'react-hot-toast';
 import { SlLogout } from 'react-icons/sl';
-
+import { FaUser } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 const LogOut = () => {
     const state = useAuthStore();
+    const navigate = useNavigate();
     return (
-        <>
-            {!!state.token && (
-                <div
-                    className='flex items-center gap-1.5 cursor-pointer'
-                    onClick={() => {
-                        state.logout();
-                        toast.success('Успешно вышли с аккаунта!');
-                    }}
-                >
-                    <span className='text-gray-600 dark:text-white'>выйти</span>
-                    <SlLogout size={30} className='fill-gray-600 dark:fill-white' />
-                </div>
+        <div
+            className='flex items-center gap-1.5 cursor-pointer'
+            onClick={() => {
+                if (state.token) {
+                    state.logout();
+                    toast.success('Успешно вышли с аккаунта!');
+                } else {
+                    navigate('/login');
+                }
+            }}
+        >
+            <span className='text-gray-600 dark:text-white'>{state.token ? 'выйти' : 'войти'}</span>
+            {state.token ? (
+                <SlLogout size={30} className='fill-gray-600 dark:fill-white' />
+            ) : (
+                <FaUser size={30} className='fill-gray-600 dark:fill-white' />
             )}
-        </>
+        </div>
     );
 };
 
